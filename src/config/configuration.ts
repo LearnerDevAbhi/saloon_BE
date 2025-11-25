86e312b4e63cf1dc9f8ff734a92727070944c44f
LearnerDevAbhi <tiwariabhishek1348@gmail.com>
2025-11-25 14:43:47 +0530

    Fix change

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,12 +3,7 @@ export default () => ({
     port: parseInt(process.env.PORT ?? '3000', 10),
   },
   database: {
-    host: process.env.DB_HOST ?? 'localhost',
     url:process.env.DATABASE_URL ?? '',
-    port: parseInt(process.env.DB_PORT ?? '5432', 10),
-    username: process.env.DB_USERNAME ?? 'postgres',
-    password: process.env.DB_PASSWORD ?? 'postgres',
-    name: process.env.DB_NAME ?? 'salon',
     logging: process.env.DB_LOGGING === 'true',
     ssl: process.env.DB_SSL === 'true',
   },
